f5065b06a9bacfd7d78b84ec1d5837819de74ab1
r0b0tx <jeremy.pro.lp@gmail.com>
2020-09-27 18:40:29 +0200

    commito

--- layout/left-panel/action-bar.lua
+++ layout/left-panel/action-bar.lua
@@ -54,7 +54,7 @@ return function(s, panel, action_bar_width)
 	panel:connect_signal(
 		'closed',
 		function()
-			menu_icon.menu_btn:set_image(gears.surface(icons.search))
+			menu_icon.menu_btn:set_image(gears.surface(icons.main_menu))
 		end
 	)
 
